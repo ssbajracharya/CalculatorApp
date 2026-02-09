669a358bbdfb3e0853b9de2c3d6b73f15623aa30
Saumya <ssbajracharya@outlook.com>
2026-02-08 18:34:47 -0600

    Add project files.

---
+++ CalculatorMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorApp
+{
+    public interface ICalculator
+    {
+        double Add(double a, double b);
+        double Subtract(double a, double b);
+        double Multiply(double a, double b);
+        double Divide(double a, double b);
+    }
+
+
+    public class CalculatorMath : ICalculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException("Cannot divide by zero");
+
+            return a / b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿namespace CalculatorApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private readonly ICalculator calculator;
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            calculator = new CalculatorMath();
+        }
+
+
+        public void OnAddClicked(object sender, EventArgs e)
+        {
+            double n1;
+            double n2;
+
+            bool isFirstValid = double.TryParse(Input1.Text, out n1);
+            bool isSecondValid = double.TryParse(Input2.Text, out n2);
+
+            if (isFirstValid && isSecondValid)
+            {
+               
+                double result = calculator.Add(n1, n2);
+                ResultLabel.Text = $"Result: {n1} + {n2} = {result}";
+            }
+            else
+            {
+                ResultLabel.Text = "Error: Please enter valid numbers only.";
+            }
+        }
+
+        public void OnSubtractClicked(object sender, EventArgs e)
+        {
+            double n1;
+            double n2;
+
+            bool isFirstValid = double.TryParse(Input1.Text, out n1);
+            bool isSecondValid = double.TryParse(Input2.Text, out n2);
+
+            if (isFirstValid && isSecondValid)
+            {
+
+                double result = calculator.Subtract(n1, n2);
+                ResultLabel.Text = $"Result: {n1} - {n2} = {result}";
+            }
+            else
+            {
+                ResultLabel.Text = "Error: Please enter valid numbers only.";
+            }
+
+        }
+
+        public void OnMultiplyClicked(object sender, EventArgs e)
+        {
+            double n1;
+            double n2;
+
+            bool isFirstValid = double.TryParse(Input1.Text, out n1);
+            bool isSecondValid = double.TryParse(Input2.Text, out n2);
+
+            if (isFirstValid && isSecondValid)
+            {
+
+                double result = calculator.Multiply(n1, n2);
+                ResultLabel.Text = $"Result: {n1} x {n2} = {result}";
+            }
+            else
+            {
+                ResultLabel.Text = "Error: Please enter valid numbers only.";
+            }
+
+        }
+
+        public void OnDivideClicked(object sender, EventArgs e)
+        {
+            double n1;
+            double n2;
+
+            bool isFirstValid = double.TryParse(Input1.Text, out n1);
+            bool isSecondValid = double.TryParse(Input2.Text, out n2);
+
+            if (isFirstValid && isSecondValid)
+            {
+                try
+                {
+                    double result = calculator.Divide(n1, n2);
+                    ResultLabel.Text = $"Result: {n1} / {n2} = {result}";
+                }catch (Exception ex)
+                {
+                    ResultLabel.Text = "Error: " + ex.Message;
+                }
+                
+            }
+
+            else
+            {
+                ResultLabel.Text = "Error: Please enter valid numbers only.";
+            }
+
+        }
+
+
+    }
+}
